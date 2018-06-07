1e91d8ce4f383f22a31da1ce1f77a0bb522b5398
Nick Clear <nickclear22@gmail.com>
2018-06-06 20:57:30 -0700

    added npm packages. required needed packages. first step at myTweets function.

--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
+var request = require("request");
 var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -12,3 +15,22 @@ var client = new Twitter(keys.twitter);
     // spotify-this-song
     // movie-this
     // do-what-it-says
+
+
+
+function myTweets(){
+    var params = {screen_name: 'nickclear'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+}
+
+//========================================================================================================================
+// TWITTER API
+//========================================================================================================================
+
+if(process.argv[2] === 'my-tweets'){
+    myTweets();
+};
